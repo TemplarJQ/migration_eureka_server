62233a32fa1ae46565eaf1aefa7f15e0cd392fef
TemplarJQ <jqliu_bupt@163.com>
2021-05-13 17:32:37 +0800

    fix rpc call

--- src/main/java/com/EurekaApplication.java
+++ src/main/java/com/EurekaApplication.java
@@ -2,17 +2,28 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * 服务端启动App
  *
  */
 
-@EnableEurekaServer
+@EnableEurekaClient
 @SpringBootApplication
 public class EurekaApplication
 {
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        // 客户端负载均衡
+        return new RestTemplate();
+    }
+
     public static void main( String[] args )
     {
         SpringApplication.run(EurekaApplication.class, args);
---
+++ src/main/java/com/config/RpcApiConfig.java
@@ -0,0 +1,9 @@
+package com.config;
+
+/**
+   @code 客户端服务端的调用接口常量（即手动配置中心，可以替换为Apollo或Spring Clouds原生组件）
+ */
+public class RpcApiConfig {
+
+    //
+}
