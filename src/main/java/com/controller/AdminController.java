d3704b2783f7541a5fab0557d52ff706d08b3981
TemplarJQ <jqliu_bupt@163.com>
2021-06-30 11:23:40 +0800

    add admin code

--- src/main/java/com/controller/AdminController.java
+++ src/main/java/com/controller/AdminController.java
@@ -1,4 +1,14 @@
 package com.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller("admin")
+@RequestMapping("/admin")
+//对跨域请求参数进行设置保证session中的信息跨域得到读取
+@CrossOrigin(allowCredentials = "true",allowedHeaders = "*")
 public class AdminController extends BaseController {
+
+
 }
--- src/main/java/com/controller/TestAPIController.java
+++ src/main/java/com/controller/TestAPIController.java
@@ -7,7 +7,6 @@ import com.dataobject.UserDO;
 import com.response.CommonReturnType;
 import com.service.RpcCallService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
